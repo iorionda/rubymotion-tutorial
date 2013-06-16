345fe58c3fbf0b97eb8816fd8c4e4200ef2074e6
Iori ONDA <iori.onda@gmail.com>
2013-06-16 19:56:17 +0900

    Add UINavigationController

--- HelloMotion/app/app_delegate.rb
+++ HelloMotion/app/app_delegate.rb
@@ -4,9 +4,8 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    # This is our new line!
-    @window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
-
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
     true
   end
 end
