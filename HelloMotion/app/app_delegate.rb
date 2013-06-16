21569e83e9421e9f1479dc1a1af8db22aa4b6bf1
Iori ONDA <iori.onda@gmail.com>
2013-06-16 19:12:08 +0900

    Done controllers

--- HelloMotion/app/app_delegate.rb
+++ HelloMotion/app/app_delegate.rb
@@ -4,19 +4,9 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
-    @blue_view.backgroundColor = UIColor.blueColor
-    @window.addSubview(@blue_view)
+    # This is our new line!
+    @window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
 
-    @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-    @green_view.backgroundColor = UIColor.greenColor
-    @window.addSubview(@green_view)
-
-    @red_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-    @red_view.backgroundColor = UIColor.redColor
-
-    # NOTE: *not* adding to @window
-    @blue_view.addSubview(@red_view)
     true
   end
 end
