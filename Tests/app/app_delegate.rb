53aabc4176585a74648997f4ab24b050a9a425e1
Iori ONDA <iori.onda@gmail.com>
2013-06-17 01:08:44 +0900

    Add window

--- Tests/app/app_delegate.rb
+++ Tests/app/app_delegate.rb
@@ -1,5 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window.makeKeyAndVisible
     true
   end
 end
